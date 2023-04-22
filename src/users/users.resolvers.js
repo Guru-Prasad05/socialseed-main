741213e607da93e45bbfc5b6f0e9859d5393fdf9
Guru-Prasad05 <113578899+Guru-Prasad05@users.noreply.github.com>
2023-04-23 03:25:47 +0530

    social seed

--- src/users/users.resolvers.js
+++ src/users/users.resolvers.js
@@ -7,7 +7,7 @@ module.exports = {
         where: {
           followers: {
             some: {
-              id,
+              userId:id,
             },
           },
         },
@@ -18,7 +18,7 @@ module.exports = {
         where: {
           following: {
             some: {
-             id,
+             followerId:id,
             },
           },
         },
